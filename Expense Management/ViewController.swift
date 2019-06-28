73e4f9667f2f569ef67fe47e6fe3ca44eb82541c
Thai Quoc Hoang <quocthai9120@gmail.com>
2019-06-28 21:36:12 +0700

    Initial Commit

---
+++ Expense Management/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Expense Management
+//
+//  Created by Hoang Quoc Thai  on 6/28/19.
+//  Copyright © 2019 ThaiHoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
